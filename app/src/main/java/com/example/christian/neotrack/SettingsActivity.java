80615db8f40666c75864cbc1c84daea35add4077
ccintrano <christiancintranolopez@gmail.com>
2016-02-04 16:47:07 +0100

    Display settings correctly

--- app/src/main/java/com/example/christian/neotrack/SettingsActivity.java
+++ app/src/main/java/com/example/christian/neotrack/SettingsActivity.java
@@ -15,14 +15,8 @@ public class SettingsActivity extends PreferenceActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
 
-    @Override
-    public void onBuildHeaders(List<Header> target) {
-        loadHeadersFromResource(R.xml.preference_headers, target);
-    }
-    @Override
-    protected boolean isValidFragment(String fragmentName) {
-        return SettingsFragment.class.getName().equals(fragmentName);
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment()).commit();
     }
 }
